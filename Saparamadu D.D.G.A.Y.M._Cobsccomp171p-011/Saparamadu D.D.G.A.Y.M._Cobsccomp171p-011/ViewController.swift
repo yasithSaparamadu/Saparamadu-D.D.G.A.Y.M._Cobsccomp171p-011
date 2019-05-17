f668ed4017be92e672226779ef877fcf87276d1f
Yasith Maheshka Saparamadu <>
2019-05-17 17:58:56 +0530

    Create project

---
+++ Saparamadu D.D.G.A.Y.M._Cobsccomp171p-011/Saparamadu D.D.G.A.Y.M._Cobsccomp171p-011/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Saparamadu D.D.G.A.Y.M._Cobsccomp171p-011
+//
+//  Created by Yasith Maheshka Saparamadu on 5/17/2562 BE.
+//  Copyright © 2562 BE Yasith Maheshka Saparamadu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
