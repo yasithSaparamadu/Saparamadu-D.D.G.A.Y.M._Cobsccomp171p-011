5ae0da14b75d9e51477b28e0ec1ec49b6b2f92ac
Yasith Maheshka Saparamadu <>
2019-05-26 19:18:49 +0530

    Fingerprint Authentication

--- Saparamadu D.D.G.A.Y.M._Cobsccomp171p-011/Saparamadu D.D.G.A.Y.M._Cobsccomp171p-011/controllers/AuthenticationViewController.swift
+++ Saparamadu D.D.G.A.Y.M._Cobsccomp171p-011/Saparamadu D.D.G.A.Y.M._Cobsccomp171p-011/controllers/AuthenticationViewController.swift
@@ -7,18 +7,49 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseAuth
 
 class AuthenticationViewController: UIViewController {
     
-    
+    let IDauth = BiometricIDAuth()
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
+   
+    func touchIdLoginAction(){
+        
+        IDauth.authenticateUser(){ [weak self] message in
+            if let message = message {
+                
+                let alertView = UIAlertController(title: "Authentication Error", message: message, preferredStyle: .alert)
+                
+                let okAction = UIAlertAction(title: "Ok", style: .default, handler: {(action:UIAlertAction!) in
+                    
+                })
+                
+                alertView.addAction(okAction)
+                self!.present(alertView, animated: true)
+            }else{
+                
+                ApptempData.userDataHaddle = Auth.auth().addStateDidChangeListener{(auth, user) in
+                    if user == nil{
+                        //self!.performSegue(withIdentifier: "<#T##String#>", sender: <#T##Any?#>)
+                    }else{
+                        self!.performSegue(withIdentifier: "touchID", sender: nil)
+                    }
+                }
+            }
+        }
+        
+    }
+    
     @IBAction func fingerprintAuth(_ sender: Any) {
         
+        self.touchIdLoginAction()
         
         
     }
